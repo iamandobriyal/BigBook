f53e10b904dbaaabc737615bc2632e78ffa71781
Aman Dobriyal <iamandobriyal@gmail.com>
2021-11-13 18:27:58 +0530

    updates shop page and login

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ let ham = document.querySelector('#ham');
 let overlay = document.querySelector('.overlay');
 let close = document.querySelector('#close');
 let loginBtn = document.querySelectorAll('#login-btn');
+let nav = document.querySelectorAll('li');
+let section = document.querySelectorAll('section');
+let shopbtn = document.querySelector('#shopnow');
+
+
 ham.addEventListener('click',function()
 {
     mobMenu.style.display = "flex";
@@ -23,3 +28,34 @@ for(let i=0; i<loginBtn.length; i++)
         location.href = "../pages/login.html";
     })
 }
+for(let i=0; i<nav.length; i++)
+{
+    nav[i].addEventListener('click',function(){
+        if(i<4)
+        {
+            section[i].style.display = "flex";
+            for(let j=0; j<section.length; j++)
+            {
+                if(j!=i)
+                {
+                    section[j].style.display = "none";
+                }
+            }
+        }
+        else if(i>3)
+        {
+            section[i-4].style.display = "flex";
+            for(let j=0; j<section.length; j++)
+            {
+                if(j!=i-4)
+                {
+                    section[j].style.display = "none";
+                }
+            }
+        } 
+    })
+}
+shopbtn.addEventListener('click',function(){
+    section[0].style.display = "none";
+    section[1].style.display = "flex";
+})
