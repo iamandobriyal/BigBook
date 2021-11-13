1ada56f2d19b829fac182cb8e954fba8d6c3f3aa
Aman Dobriyal <iamandobriyal@gmail.com>
2021-11-13 19:01:22 +0530

    login form changes

--- js/login.js
+++ js/login.js
@@ -2,8 +2,10 @@ let signupbtn = document.querySelector('#signup');
 let signinbtn = document.querySelector('#signin');
 let login = document.querySelector('#login');
 let register = document.querySelector('#register');
-let buttons = document.querySelectorAll('buttons');
+let buttons = document.querySelectorAll('button');
 let fields = document.querySelectorAll('input');
+let forget = document.querySelector('#fr');
+let stch = document.querySelector('.switch');
 signupbtn.addEventListener('click',function()
 {
     signupbtn.classList.add('active');
@@ -25,4 +27,11 @@ buttons[1].addEventListener('click',function()
     {
         document.querySelector('output').textContent = "Password Does Not Match";
     }
+})
+
+forget.addEventListener('click',function(){
+    document.querySelector('#forget').style.display = "flex";
+    login.style.display = "none";
+    register.style.display = "none";
+    stch.style.display = "none";
 })
